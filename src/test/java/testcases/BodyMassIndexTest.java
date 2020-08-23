2915e48a1a6ae898e38c8c23e67049ddf94921aa
Nguyen Thi Bich Thuy <ntbichthuy07@gmail.com>
2020-08-23 23:23:14 +0700

    data Provider and demo annotations

--- src/test/java/testcases/BodyMassIndexTest.java
+++ src/test/java/testcases/BodyMassIndexTest.java
@@ -3,6 +3,7 @@ package testcases;
 import functions.BmiCalculatorFunction;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -17,13 +18,28 @@ public class BodyMassIndexTest {
         visit(url);
     }
 
-    @Test(description = "Validate bmi calculator in metric unit")
-    public static void metricUnit() {
+    @DataProvider(name = "bmiTestdata")
+    public static Object[][] testData() {
+        return new Object[][]{
+                {"25", "male", "180", "35", "BMI = 10.8 kg/m2   (Severe thinness)"},
+                {"25", "male", "180", "55", "BMI = 17 kg/m2   (Moderate thinness)"},
+                {"25", "male", "180", "60", "BMI = 18.5 kg/m2   (Mild thinness)"},
+                {"25", "male", "180", "70", "BMI = 21.6 kg/m2   (Normal)"},
+                {"25", "male", "180", "85", "BMI = 26.2 kg/m2   (Overweight)"},
+                {"25", "male", "180", "100", "BMI = 30.9 kg/m2   (Obese Class I)"},
+                {"25", "male", "180", "120", "BMI = 37 kg/m2   (Obese Class II)"},
+                {"25", "male", "180", "150", "BMI = 46.3 kg/m2   (Obese Class III)"},
+
+        };
+    }
+
+    @Test(description = "Validate bmi calculator in metric unit", dataProvider = "bmiTestdata")
+    public static void perform(String age, String gender, String height, String weight, String expectedResult) {
         BmiCalculatorFunction calculatorPage = new BmiCalculatorFunction();
         calculatorPage.selectMetricTab();
-        calculatorPage.fillForm("33", "male", "172", "65");
+        calculatorPage.fillForm(age, gender, height, weight);
 
-        Assert.assertEquals(calculatorPage.getResult(), "BMI = 22 kg/m2   (Normal)");
+        Assert.assertEquals(calculatorPage.getResult(), expectedResult);
 
     }
 }
--- src/test/java/testcases/SampleTestSuite.java
+++ src/test/java/testcases/SampleTestSuite.java
@@ -1,10 +1,31 @@
 package testcases;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class SampleTestSuite {
 
-    @Test(groups = {"smoke","firefox"})
+    @BeforeSuite
+    void beforeSuite(){
+        System.out.println("before suite");
+    }
+    @BeforeTest
+    void beforeTest(){
+        System.out.println("before test");
+    }
+    @BeforeClass
+    void beforeClass(){
+        System.out.println("before class");
+    }
+    @BeforeGroups
+    void beforeGroup(){
+        System.out.println("before group");
+    }
+    @BeforeMethod
+    void beforeMethod(){
+        System.out.println("before method");
+    }
+
+    @Test
     void tc01(){
         System.out.println("the first test");
     }
@@ -19,12 +40,32 @@ public class SampleTestSuite {
         System.out.println("the third test");
     }
 
-    @Test(groups = {"smoke","ie"})
+    @Test
     void tc04(){
         System.out.println("the fourth test");
     }
     @Test
     void tc05(){
         System.out.println("the fifth test");
+    }
+
+    @AfterMethod
+    void afterMethod(){
+        System.out.println("after method");
+    }
+
+    @AfterClass
+    void afterClass(){
+        System.out.println("after class");
+    }
+
+    @AfterTest
+    void afterTest(){
+        System.out.println("after test");
+    }
+
+    @AfterSuite
+    void afterSuite(){
+        System.out.println("after suite");
     }
 }
